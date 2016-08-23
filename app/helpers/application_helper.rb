e635bd5086f7e4ecc79479c0632c77bfb9cb22bf
huaxiukun <huaxiukun@yiyc.com>
2016-08-23 10:44:50 +0800

    config icon site-name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_page_title
+    site_name = Settings.site_name
+    title = @page_title ? "#{@page_title} - #{site_name}" : site_name
+    content_tag('title', title, nil, false)
+  end
 end
