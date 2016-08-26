40b7ba0dbcd3a14464a4ecedc453a7da49d78b7e
huaxiukun <huaxiukun@yiyc.com>
2016-08-26 17:10:47 +0800

    iframe

--- app/controllers/our_cas_sessions_controller.rb
+++ app/controllers/our_cas_sessions_controller.rb
@@ -1,4 +1,4 @@
-class OurCasSessionsController < Devise::CasSessionsController
+class OurCasSessionsController < Auth::CasSessionsController
   #  skip_before_action :redirect_to_sign_in,
   #  only: [:new, :destroy, :single_sign_out,
   #         :service, :unregistered]
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     title = @page_title ? "#{@page_title} - #{site_name}" : site_name
     content_tag('title', title, nil, false)
   end
+
+  def iframe_src(path)
+    url = URI.join(Devise.cas_base_url, path)
+    url.query = URI.encode_www_form([["service", request.base_url]])
+    url
+  end
+
 end
