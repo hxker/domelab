ef2e61fc76a5850e1f504d753c31ba653f22d822
huaxiukun <huaxiukun@yiyc.com>
2016-09-06 10:49:52 +0800

    user migrate

--- db/migrate/20160823031052_create_users.rb
+++ db/migrate/20160823031052_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.integer :guid, null: false, unique: true
       t.string :nickname, null: false, unique: true
       t.string :mobile
       t.string :email
@@ -23,7 +22,6 @@ class CreateUsers < ActiveRecord::Migration[5.0]
 
       t.timestamps
     end
-    add_index :users, :guid, unique: true
     add_index :users, :nickname, unique: true
     add_index :users, :mobile
     add_index :users, :email
