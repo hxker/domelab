d2f510df30c7c712dd8503cd42fc50545b1294cb
huaxiukun <huaxiukun@yiyc.com>
2016-09-29 11:23:59 +0800

    course name

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -111,7 +111,7 @@ class CoursesController < ApplicationController
     if lesson_id.present? && check_ability.present?
       @tests = LessonTest.where(lesson_id: lesson_id)
       @has_test = current_user.user_lesson_tests.find_by(lesson_id: lesson_id)
-      @lesson_name = Lesson.find(lesson_id).name
+      @belong_name = Lesson.joins(:course).where(id: lesson_id).select(:name, 'courses.name as course_name').take
     else
       render_optional_error(403)
     end
