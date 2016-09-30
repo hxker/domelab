33636e927f9f1f7edb58fe822decf9fa4d7d6693
WHT <wht@WHTdeMacBook-Pro.local>
2016-09-30 17:03:59 +0800

    schedule

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -2,11 +2,11 @@
  * Created by huaxiukun on 16/9/26.
  */
 
-$(function () {
+$(function() {
     var start_time = Date.now();
     var user_lesson_test = $('#user-lesson-test-submit');
     if (user_lesson_test.length > 0) {
-        user_lesson_test.on('click', function () {
+        user_lesson_test.on('click', function() {
             var lesson_id = $('#course-lesson-id').val();
             var test_ids = $('#lesson-test-ids').val().split(',');
             var answers = {};
@@ -22,23 +22,28 @@ $(function () {
                 }
             }
             if (answers && getJsonLength(answers) == test_ids.length) {
-                var ms = start_time-Date.now();
-                    min = (ms/1000/60) << 0,
-                    sec = (ms/1000) % 60;
+                var ms = start_time - Date.now();
+                min = (ms / 1000 / 60) << 0,
+                    sec = (ms / 1000) % 60;
                 $.ajax({
                     url: '/courses/check_lesson_test',
                     type: 'post',
-                    data: {answers: answers, lesson_id: lesson_id},
-                    success: function (data) {
+                    data: {
+                        answers: answers,
+                        lesson_id: lesson_id
+                    },
+                    success: function(data) {
                         console.log(data);
-                        if (data[0]==true) {
+                        if (data[0] == true) {
                             // var html = $('<div><h1>正确率:' + data[1]["right_per"] + '</h1><p><img src="' + data[1]["teacher_avatar"] + '"></p>' +
                             //     '<p><a class="btn btn-xs btn-primary" href="javascript:history.go(-1);">返回</a></p></div>');
                             // $('#lesson-test-container').html(html)
                             $('#lesson-test-container').addClass("hidden");
                             $('#timepassed span').text(min + ':' + sec);
                             $('#test-result span').text(data[1]["right_per"]);
-                            $('test-teacher-wrapper').aapend($('<img>',{src:data[1]["teacher_avatar"]}));
+                            $('test-teacher-wrapper').aapend($('<img>', {
+                                src: data[1]["teacher_avatar"]
+                            }));
                             $('#test-result-container').removeClass("hidden");
                         } else {
                             alert(data[1])
@@ -50,9 +55,9 @@ $(function () {
 
         });
     }
-    scheduler.init('scheduler_here', new Date(),"month");
-    var data= $('.scheduler-data').data('scheduler');
+    initScheduler();
 });
+
 function getJsonLength(jsonObj) {
 
     var jsonLength = 0;
@@ -60,4 +65,46 @@ function getJsonLength(jsonObj) {
         jsonLength++;
     }
     return jsonLength;
+}
+
+function initScheduler() {
+    function formate_date(datestring) {
+        var d = new Date(datestring);
+        var fd = [d.getMonth() + 1, "/", d.getDate(), "/", d.getFullYear()].join('')
+        return fd
+    }
+    scheduler.config.start_on_monday = false;
+    scheduler.config.readonly = true;
+    scheduler.config.month_day = "%j";
+    scheduler.config.week_date = "%D";
+    scheduler.templates.month_date = function(date) {
+        var d = new Date(date);
+        var month = d.getMonth() + 1;
+        $(".month-list a").removeClass("selected");
+        $(".month-list li:nth-child(" + month + ") a").addClass("selected");
+        return d.getFullYear();
+    };
+    scheduler.init('scheduler_here', new Date(), "month");
+    scheduler.templates.event_bar_date = function(start, end, ev) {
+        return "";
+    };
+    scheduler.templates.event_bar_text = function(start, end, event) {
+        return "<div class='course'>" + event.text + "</div>" + "<div class='lesson'>" + event.text + "</div>";
+    };
+
+    var data = $('.scheduler-data').data('scheduler');
+    var final_data = data.map(function(obj) {
+        return {
+            text: obj.title,
+            start_date: formate_date(obj.start),
+            end_date: formate_date(obj.end)
+        }
+    });
+    scheduler.parse(final_data, "json");
+    $(".month-list li").click(function() {
+        var year = $(".dhx_cal_date").text();
+        var month = $(".month-list li").index(this);
+        scheduler.setCurrentView(new Date(year, month, 1), "month");
+    });
+
 }
