82307c50043e7b7938bbb8da1723b5f1349319d1
huaxiukun <huaxiukun@yiyc.com>
2016-10-17 15:36:48 +0800

    home course date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    courses = Course.where(status: 1)
+    @dome_courses = courses.where(course_type: 0).limit(3)
+    @expert_courses = courses.where(course_type: 1).limit(3)
 
   end
 
