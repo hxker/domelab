8cf4c27bc36f2223d5581aa5f93ac17bbad57856
WHT <wht@WHTdeMacBook-Pro.local>
2016-10-25 13:50:43 +0800

    fix carousel index

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,6 +1,23 @@
 $(function() {
-    $('#carousel').on('slide.bs.carousel', function() {
-        var currentIndex = $(this).find('.active').index() + 1;
-        $(".control .index").text(currentIndex + 1);
+    var length = $('.carousel-inner .item').length;
+    $('#carousel a.left,#carousel-left').on('click', function() {
+        var index;
+        var currentIndex = $('#carousel').find('.active').index();
+        if (currentIndex === 0) {
+            index = length - 1;
+        } else {
+            index = currentIndex - 1;
+        }
+        $(".control .index").text(index + 1);
+    });
+    $('#carousel a.right,#carousel-right').on('click', function() {
+        var index;
+        var currentIndex = $('#carousel').find('.active').index();
+        if (currentIndex === length - 1) {
+            index = 0;
+        } else {
+            index = currentIndex + 1;
+        }
+        $(".control .index").text(index + 1);
     });
 });
