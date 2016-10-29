d3a78a935b2fbb507e2c0a9dd5dcadfa9e6dc76d
WHT <wht@WHTdeMacBook-Pro.local>
2016-10-29 12:14:17 +0800

    fix lesson_ware link in show page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,7 +13,7 @@ class CoursesController < ApplicationController
       if course.course_type == 0
         @course = Course.joins(:group_course_ships).joins('left join group_user_ships g_u on g_u.group_id = group_course_ships.group_id').where(id: course_id).where('g_u.user_id=?', current_user.id).select(:id, :name, :cover, :course_type, :course_info, 'group_course_ships.group_id').take
         if @course
-          @lessons = Lesson.where(course_id: course_id)
+          @lessons = Lesson.where(course_id: course_id).includes(:photos)
           @stars = course.course_stars.select(:name, :stars)
         else
           render_optional_error(403)
