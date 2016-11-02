a8a85a2311c05e59fd4094210ebbc0accf4e6ab0
WHT <wht@WHTdeMacBook-Pro.local>
2016-11-02 13:07:50 +0800

    add lesson test link to the end of lessonware

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -1,22 +1,29 @@
 $(function() {
+    var lesson_test = $("#lesson-test-link");
     var length = $('.carousel-inner .item').length;
     $('#carousel a.left,#carousel-left').on('click', function() {
         var index;
         var currentIndex = $('#carousel').find('.active').index();
         if (currentIndex === 0) {
             index = length - 1;
+            lesson_test.removeClass('hidden');
         } else {
             index = currentIndex - 1;
+            lesson_test.addClass('hidden');
         }
         $(".control .index").text(index + 1);
     });
     $('#carousel a.right,#carousel-right').on('click', function() {
+        lesson_test.addClass('hidden');
         var index;
         var currentIndex = $('#carousel').find('.active').index();
         if (currentIndex === length - 1) {
             index = 0;
         } else {
             index = currentIndex + 1;
+            if (index === length - 1) {
+                lesson_test.removeClass('hidden');
+            }
         }
         $(".control .index").text(index + 1);
     });
